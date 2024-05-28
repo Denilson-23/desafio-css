717f42c237d56aa4877b30188685b45621632e77
Denilson-23 <denilsonmorales45@gmail.com>
2024-05-28 00:07:46 -0300

    funcao colocar na tela

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 let tela = ("Passeios compartilhados. Há muito o que fazer na sua cidade e nós temos os passeios para ajudar você a aproveitar tudo. E eles estão todos disponíveis diretamente no seu aplicativo")
 let tela2=("Localização de ampla cobertura")
-
-
+let elemento=document.querySelector("p")
+let elemento2=document.querySelector("h1")
 function colocarNaTela() {
 
-    document.querySelector("p").innerHTML = tela
-    document.querySelector("h1").innerHTML = tela2
+    elemento.innerHTML = tela
+    elemento2.innerHTML = tela2
 
     console.log(tela)
     console.log(tela2)
@@ -22,4 +22,14 @@ function cliqueiNoLink() {
 
     colocarNaTela()
 
+}
+
+
+
+
+
+function cliqueiNoBotao (){
+    let inputt=document.querySelector(".input1").value
+    console.log(inputt)
+    
 }
